1e1727399de95d36b4893624ff31e0da4899dd49
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-18 23:52:28 +0100

    Add default files

---
+++ start_elasticsearch_server.sh
@@ -0,0 +1,14 @@
+ES_PORT=9200
+ES_BIN=/home/jurbani/wdps/elasticsearch-2.4.1/bin/elasticsearch
+
+>.es_log*
+prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
+echo "waiting for elasticsearch to set up..."
+until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
+ES_PID=$!
+until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
+echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
+
+python3 elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
+
+kill $ES_PID
