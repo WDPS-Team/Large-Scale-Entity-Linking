b70a884fbd67315181994ba2848ef9329f580c0b
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-22 16:18:11 +0100

    Add first warc split reader to spark

--- app/src/spark_main.py
+++ app/src/spark_main.py
@@ -1,5 +1,26 @@
 from pyspark import SparkContext, SparkFiles
 
+
+class WARCSplitReader:
+
+    def __init__(self, spark_session, lines_of_input_file):
+        self.sc = spark_session
+        self.raw_lines = lines_of_input_file
+        self.docs = None
+
+    def __split_records(self):
+        payload = ''
+        for line in self.raw_lines:
+            if line.strip() == "WARC/1.0":
+                yield payload
+                payload = ''
+            else:
+                payload += " " + line
+
+    def convert_to_docs(self):
+        self.docs = self.__split_records()
+        return self.sc.parallelize(self.docs)
+
 # Initialize Spark App
 sc = SparkContext()
 
@@ -7,9 +28,11 @@ sc = SparkContext()
 input_file = sc.textFile("sample.warc.gz")
 # Space for the main program
 
+wsr = WARCSplitReader(sc, input_file.collect())
+docs_rdd = wsr.convert_to_docs()
 
 # TODO: Convert RDD into a TSV
 
 # Write the file to hdfs
-output_rdd = input_file
+output_rdd = docs_rdd
 output_rdd.repartition(1).saveAsTextFile("output/predictions.tsv")
