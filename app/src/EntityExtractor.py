d272e48ef4c4ada34b040ca56efa01d6e6a996ab
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-23 21:34:50 +0100

    add entity extraction

--- app/src/EntityExtractor.py
+++ app/src/EntityExtractor.py
@@ -2,39 +2,23 @@ import json
 import os
 import spacy
 
-from config import TMP_FOLDER
-
 class EntityExtractor:
 
-    def __init__(self, type='en'):
-        self.spacy_nlp = spacy.load(type)
+    def __init__(self, warc_docs):
+        self.warc_docs = warc_docs
     
-    def extract(self, folder):
-        files_list = os.listdir(folder)
-        entityList = []
-        
-        for file_name in files_list:
-            with open(folder + "/" + file_name) as json_file:
-                data = json.load(json_file)
-                for p in data:
-                    document = self.spacy_nlp(p['data'].strip())
-                    for element in document.ents:
-                        entity = dict(doc_id=p['id'], type=element.label_, text=element.text)
-                        entityList.append(entity)
-        return entityList
+    def extract(self):
 
+        def process(row):
+            # TODO: change to bigger model
+            spacy_nlp = spacy.load("en_core_web_sm")
+            document = spacy_nlp(row['data'].strip())
+            entity_list = []
+            for element in document.ents:
+                entity = dict(type=element.label_, text=element.text)
+                entity_list.append(entity)
 
-if __name__ == "__main__":
-    import sys
+            return {"doc_id": row["id"], "entities": entity_list}
 
-    if len(sys.argv) < 2:
-        print('Usage: <INPUT_FOLDER>. Using "',TMP_FOLDER,'" instead.')
-        INPUT = TMP_FOLDER
-    else:
-        INPUT = sys.argv[1]
-
-    entities = EntityExtractor('en')
-    entityList = entities.extract(INPUT)
-
-    for entity in entityList:
-        print(entity)
+        docs_with_entities = self.warc_docs.map(process)
+        return docs_with_entities
--- app/src/spark_main.py
+++ app/src/spark_main.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext, SparkFiles
 from WARCSplitReader import WARCSplitReader
+from EntityExtractor import EntityExtractor
 
 # Initialize Spark App
 sc = SparkContext()
@@ -13,12 +14,19 @@ wsr.parse_warc_records()
 wsr.process_warc_records()
 wsr.filter_invalid_records()
 cleaned_warc_records = wsr.clean_warc_responses()
+docs = cleaned_warc_records
+# TODO: remove on final product
+cleaned_warc_records = sc.parallelize(cleaned_warc_records.take(10))
+
+# STAGE 2 - Entity Extraction
+ee = EntityExtractor(cleaned_warc_records)
+docs = ee.extract()
 
 # to be filled
 
 # STAGE OUTPUT - Writing as TSV
 # TODO: actually write as TSV
-docs_rdd = cleaned_warc_records
+docs_rdd = docs
 
 print("row count: {0}".format(docs_rdd.count()))
 
