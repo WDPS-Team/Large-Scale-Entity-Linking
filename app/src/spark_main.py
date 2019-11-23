51c03fbb2acdafa5b58a6516e6177acc87a0afaf
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-23 14:39:54 +0100

    Clean up main script

--- app/src/spark_main.py
+++ app/src/spark_main.py
@@ -1,29 +1,26 @@
 from pyspark import SparkContext, SparkFiles
-import spacy
-import config
 from WARCSplitReader import WARCSplitReader
 
 # Initialize Spark App
 sc = SparkContext()
 
-print("using versions")
-print("spacy: {0}".format(spacy.__version__))
-print(config.WARC_ID)
-
-# Read the input file as a rdd
+# STAGE 1 - INPUT READING
+# -> READ warc files in a distributed manner
+# -> Clean all warc records (js, style) with lxml
 input_file = sc.textFile("sample.warc.gz")
-# Space for the main program
-
 wsr = WARCSplitReader(sc, input_file.collect())
-
-docs_rdd = wsr.parse_warc_records()
-docs_rdd = wsr.process_warc_records()
+wsr.parse_warc_records()
+wsr.process_warc_records()
 wsr.filter_invalid_records()
-docs_rdd = wsr.clean_warc_responses()
+cleaned_warc_records = wsr.clean_warc_responses()
+
+# to be filled
+
+# STAGE OUTPUT - Writing as TSV
+# TODO: actually write as TSV
+docs_rdd = cleaned_warc_records
+
 print("row count: {0}".format(docs_rdd.count()))
 
-# TODO: Convert RDD into a TSV
-
-# Write the file to hdfs
 output_rdd = docs_rdd
 output_rdd.repartition(1).saveAsTextFile("output/predictions.tsv")
