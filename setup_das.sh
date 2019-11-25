880e8486ae52db923ff8c77dbca443e34c04e073
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-25 08:15:40 +0100

    fix setup sh

--- setup_das.sh
+++ setup_das.sh
@@ -2,10 +2,9 @@
 
 echo "loading binaries"
 module load prun # DAS4 instance management
-if [ $? -eq 0 ] then echo "all done loading prun" else echo "something went wrong" exit 1 fi
+if [ $? -eq 0 ]; then echo "all done loading prun"; else echo "something went wrong" && exit 1; fi
 module load hadoop # Hadoop stuff
-if [ $? -eq 0 ] then echo "all done loading hadoop" else echo "something went wrong" exit 1 fi
+if [ $? -eq 0 ]; then echo "all done loading hadoop"; else echo "something went wrong" && exit 1; fi
 
 echo "copy binaries"
 cp -r /home/jurbani/wdps/elasticsearch-2.4.1 /home/wdps1936/elasticsearch-2.4.1
-
