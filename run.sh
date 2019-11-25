d6d1b057f505411b01247f975d9164a446061c10
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-25 23:48:00 +0100

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,14 @@
-# TODOS:
-# Add parameters for input and output files
-
-# Prerequsites:
-# - setup DAS4 cluster
-
-# Cleanup Files
+# Delete output files prior run
 rm ./output.tsv
 hdfs dfs -rm -r output/predictions.tsv
 
-# PYSPARK_PYTHON=$(readlink -f $(which python3)) /home/bbkruit/spark-2.1.2-bin-without-hadoop/bin/spark-submit \
-# --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
-# --master yarn \
-# --archives venv.zip#VENV \
-# app/src/spark.py app/data/sample.warc.gz ./log
-
-# PYSPARK_PYTHON=$(readlink -f $(which python3)) /local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
-# --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
-# --master yarn \
-# --archives venv.zip#VENV \
-# app/src/spark.py
-
+# Run Spark Job
 PYSPARK_PYTHON=$(readlink -f $(which python3)) /local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
 --conf spark.pyspark.virtualenv.enabled=true \
 --conf spark.pyspark.virtualenv.type=native \
 --conf spark.pyspark.virtualenv.requirements=requirements.txt \
 --conf spark.pyspark.virtualenv.bin.path=venv/bin/virtualenv \
 app/src/spark.py
-
 
 # Copying Output File from HDFS
 hdfs dfs -copyToLocal output/predictions.tsv/part-00000 ./output.tsv
