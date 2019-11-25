4ba7b7bbd4da2a1c12e1c7201403e5243a3f0a81
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-25 22:52:11 +0100

    Add WARCSplitReader

--- app/src/spark.py
+++ app/src/spark.py
@@ -1,7 +1,10 @@
 from pyspark import SparkContext, SparkFiles
+from WARCSplitReader import WARCSplitReader
+
 sc = SparkContext()
 
 import warcio
+
 print(warcio)
 
 big_list = range(10000)
@@ -9,5 +12,9 @@ rdd = sc.parallelize(big_list, 2)
 odds = rdd.filter(lambda x: x % 2 != 0)
 output = odds.take(5)
 
-print("SPARK FINISHED")
+print("SPARK FIRST STAGE FINISHED")
 print(output)
+
+input_file = sc.textFile("sample.warc.gz")
+wsr = WARCSplitReader(sc, input_file.collect())
+print(wsr.count())
