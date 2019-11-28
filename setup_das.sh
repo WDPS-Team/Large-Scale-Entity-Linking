68620b12d1a81628c8c80f0f612f83591c62ef04
Abijith <wdps1936@fs0.cm.cluster>
2019-11-28 10:02:47 +0100

    moved venv building from headnode

--- setup_das.sh
+++ setup_das.sh
@@ -18,17 +18,7 @@ if [ ! -d "/home/wdps1936/spark" ]; then
     cp -r /local/spark /home/wdps1936/
 fi
 
-echo "Building virtual environment" #TODO: Don't build venv in headnode
-rm -rf venv
-rm -rf venv.zip
-pip3 install --user virtualenv
-python3 ~/.local/lib/python3.6/site-packages/virtualenv.py -p python3 venv
-source venv/bin/activate
-pip install -r requirements.txt
-python3 -m spacy download en_core_web_sm
-virtualenv --relocatable venv
-zip -r venv.zip venv
-deactivate
+prun -v -np 1 sh setup_venv.sh
 
 echo "Copying default input file to hdfs"
 hdfs dfs -rm -r hdfs://master.ib.cluster:8020/user/wdps1936/sample.warc.gz
