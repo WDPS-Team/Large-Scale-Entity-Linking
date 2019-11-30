5f563ac7fd7b625a6d8195cbf296ad915e0f1de7
jonsnow <abijithanikkuruthi@gmail.com>
2019-11-30 19:11:09 +0100

    submit changes

--- spark/submit/scripts/submit.sh
+++ spark/submit/scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #rm -r /output/predictions.tsv
-cp /data/sample.warc.gz /input.warc.gz
+cp /data/sample.warc.gz /sample.warc.gz
 
 source venv/bin/activate
 # Run Spark Job
@@ -9,7 +9,7 @@ PYSPARK_PYTHON=$(readlink -f $(which python3)) /spark/bin/spark-submit \
 --conf spark.pyspark.virtualenv.type=native \
 --conf spark.pyspark.virtualenv.requirements=requirements.txt \
 --conf spark.pyspark.virtualenv.bin.path=venv/bin/virtualenv \
-src/spark.py --es "es01:9200"
+src/spark.py --es "es01:9200" --f "sample.warc.gz"
 
 deactivate
 rm -r /data/output/
