a8ed0b47f174421f9a85a6be9126d75efa3cd8b1
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-12-01 23:39:16 +0100

    Add export to reuse ES_PATH

--- start_elasticsearch_server.sh
+++ start_elasticsearch_server.sh
@@ -9,6 +9,6 @@ ES_PID=$!
 #until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
 echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
-ES_PATH="$ES_NODE:$ES_PORT"
+export ES_PATH="$ES_NODE:$ES_PORT"
 echo $ES_PATH > .es_path
 #python elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
