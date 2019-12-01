4b98dba4fcc113e9db354dac294be7f2fb54f80e
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-01 19:15:14 +0100

    Filter types of entity candidates

--- src/EntityExtractor.py
+++ src/EntityExtractor.py
@@ -15,8 +15,10 @@ class EntityExtractor:
             document = spacy_nlp(row['text'].strip())
             entity_list = []
             for element in document.ents:
-                entity = dict(type=element.label_, text=element.text)
-                entity_list.append(entity)
+                text = element.text.strip("\n").replace("\n", "").replace("\r", "")
+                if element.label_ not in ["CARDINAL", "DATE", "QUANTITY", "TIME", "ORDINAL", "MONEY", "PERCENT", "QUANTITY"]:
+                    entity = dict(type=element.label_, text=text)
+                    entity_list.append(entity)
 
             return {"doc_id": row["_id"], "entities": entity_list}
 
