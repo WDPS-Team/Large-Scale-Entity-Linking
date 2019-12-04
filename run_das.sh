6e5fbee304d4086f0befcb9b1f611e3d00885863
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-04 19:02:30 +0100

    trident and performance changes

--- run_das.sh
+++ run_das.sh
@@ -8,6 +8,7 @@ PYSPARK_PYTHON=./VENV/venv/bin/python3 ../spark/spark-2.4.0-bin-hadoop2.7/bin/sp
 --conf spark.pyspark.virtualenv.requirements=requirements.txt \
 --conf spark.pyspark.virtualenv.bin.path=./VENV/venv/bin/virtualenv \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
+--num-executors 84 \
 --archives venv.zip#VENV \
 src/spark.py --es "$1" --f "$2" --kb "$3"
 
