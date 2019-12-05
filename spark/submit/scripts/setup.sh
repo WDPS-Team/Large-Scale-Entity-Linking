789992b9f5593b1fe43b09524f0ed8d5b6e1e873
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-12-05 16:27:50 +0100

    Fix docker setup

--- spark/submit/scripts/setup.sh
+++ spark/submit/scripts/setup.sh
@@ -7,7 +7,7 @@ pip3 install --user virtualenv
 python3 /usr/local/lib/python3.5/dist-packages/virtualenv.py -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m spacy download en_core_web_sm
+python3 -m spacy download en_core_web_md
 virtualenv --relocatable venv
 zip -r venv.zip venv
 deactivate
