bf19968bba7f2c7240ec0d32e12fef336e321b1e
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-12-05 16:28:13 +0100

    Remove more tags

--- src/TextPreprocessor.py
+++ src/TextPreprocessor.py
@@ -4,6 +4,7 @@ import lxml.html as lh
 from lxml.html.clean import Cleaner
 import re
 
+
 class TextPreprocessor:
     def __init__(self, warc_records_rdd):
         self.warc_records = warc_records_rdd
@@ -34,7 +35,7 @@ class TextPreprocessor:
             except Exception as e:
                 print("Error Converting to LXML", row["id"], "Error: ", type(e))
                 return cleaned_result
-             # Clean
+            # Clean
             cleaned_html = clean_html(lxml_doc)
             html_doc = lh.tostring(cleaned_html)
             cleaned_result["html"] = html_doc
@@ -48,26 +49,30 @@ class TextPreprocessor:
         return self.fit_cleaned_warc_responses
 
     def extract_text_from_document(self):
-        text_remove_css_classes = ["navbar"]
-        text_remove_ids = ["menu", "footer", "nav-bar"]
+        # Not sure: main_meta -> flickr img title -> sometimes might be useful
+        text_remove_css_classes = ["navbar",  "widget", "main_meta", "feeds", "copyright"]
+        text_remove_ids = ["menu", "footer", "nav-bar", "topnav", "search", "search-bar", "header-bar", "cat-bar-wipe"]
         text_remove_tags = ["script", "head"]
+
         def extract(row):
             html = row["html"]
             soup = bs4.BeautifulSoup(html, features="lxml")
-            
+
             # Get Title
-            qry_title=soup.find_all("title")
+            qry_title = soup.find_all("title")
             if len(qry_title) != 0:
                 row["title"] = str(qry_title[0].string)
- 
+
             # Remove CSS classes:
-            removable_tags = soup.find_all(name="div", attrs={ "class": text_remove_css_classes })
-            for tag_with_css_class in removable_tags:
-                tag_with_css_class.decompose()
+            for css_class in text_remove_css_classes:
+                removable_tags = soup.find_all(attrs={"class": css_class})
+                for tag_with_css_class in removable_tags:
+                    tag_with_css_class.decompose()
             # Remove IDs:
-            removable_tags = soup.find_all(id=text_remove_ids)
-            for tag in removable_tags:
-                tag.decompose()
+            for id in text_remove_ids:
+                removable_tags = soup.find_all(id=id)
+                for tag in removable_tags:
+                    tag.decompose()
             # Remove Non-relevant tags i.e. <script>
             removable_tags = soup.find_all(text_remove_tags)
             for tag in removable_tags:
@@ -81,4 +86,4 @@ class TextPreprocessor:
 
         extract_rdd = self.cleaned_warc_responses.map(extract)
         self.extracted_text_doc_records = extract_rdd
-    
+        return self.extracted_text_doc_records
