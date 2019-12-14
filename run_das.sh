1c73d9ad89cd164f5e180d67e36954e5245b6677
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-12-14 14:40:11 +0100

    add libpath for yarn executors

--- run_das.sh
+++ run_das.sh
@@ -8,6 +8,9 @@ PYSPARK_PYTHON=./VENV/venv/bin/python3 ../spark/spark-2.4.0-bin-hadoop2.7/bin/sp
 --conf spark.pyspark.virtualenv.requirements=requirements.txt \
 --conf spark.pyspark.virtualenv.bin.path=./VENV/venv/bin/virtualenv \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
+--conf spark.executor.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
+--conf spark.driver.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
+--conf spark.yarn.am.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
 --num-executors 38 --executor-cores 2 --executor-memory 6GB \
 --archives venv.zip#VENV \
 --py-files src/LexVec.py \
