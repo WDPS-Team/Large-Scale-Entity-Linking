9fe72dfaa09157d9abf25d4440a4435b85df7322
Leonard Herold <git@hdld.me>
2019-12-17 16:04:46 +0100

    Add counts to force evaluation

--- src/spark.py
+++ src/spark.py
@@ -47,6 +47,8 @@ wsr = WARCSplitReader(sc, input_file.collect())
 wsr.parse_warc_records()
 wsr.process_warc_records()
 warc_stage_rdd = wsr.filter_invalid_records()
+warc_stage_rdd.cache()
+print("Processed: {0}".format(warc_stage_rdd.count()))
 
 # Filter to intersting records:
 if debug:
@@ -61,6 +63,7 @@ text_prepro.clean_warc_responses()
 text_prepro.extract_text_from_document()
 txtprepro_stage_rdd = text_prepro.filter_unfit_records()
 txtprepro_stage_rdd.cache()
+print("Processed: {0}".format(txtprepro_stage_rdd.count()))
 
 if debug:
     for row in txtprepro_stage_rdd.collect():
@@ -76,6 +79,7 @@ ee_stage_rdd = ee.extract()
 ee_stage_rdd.cache()
 ee_stage_rdd = ee.join_sentences()
 ee_stage_rdd.cache()
+print("Processed: {0}".format(ee_stage_rdd.count()))
 
 if debug:
     for row in ee_stage_rdd.collect():
@@ -85,19 +89,25 @@ print("STAGE 4 - Entity Linking - Candidate Generation")
 el_cg = ELCandidateGeneration(ee_stage_rdd, es_path, ranking_threshold, model_root_path)
 candidates_rdd = el_cg.get_candidates_from_elasticsearch()
 candidates_rdd.cache()
+print("Processed: {0}".format(candidates_rdd.count()))
 
 print("STAGE 5 - Entity Linking - Candidate Ranking")
 el_cr = ELCandidateRanking(candidates_rdd, kb_path, ranking_threshold, model_root_path)
 el_cr.rank_entity_candidates()
 el_cr.disambiguate_type()
 ranked_candidates_rdd = el_cr.disambiguate_label()
+ranked_candidates_rdd.cache()
+print("Processed: {0}".format(ranked_candidates_rdd.count()))
 
 print("STAGE 6 - Entity Linking - Mapping Selection")
 el_ms = ELMappingSelection(ranked_candidates_rdd)
 selected_entities_rdd = el_ms.select()
+selected_entities_rdd.cache()
+print("Processed: {0}".format(selected_entities_rdd.count()))
 
 print("STAGE 7 - Writing Output")
 ow = OutputWriter(selected_entities_rdd)
 ow_stage_rdd = ow.convert_to_tsv()
 ow_stage_rdd.cache()
+print("Processed: {0}".format(ow_stage_rdd.count()))
 ow_stage_rdd.saveAsTextFile("output/predictions.tsv")
