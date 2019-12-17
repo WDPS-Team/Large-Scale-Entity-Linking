2851c3fa8e02b9d3003278ee4e21888485b2e074
Leonard Herold <git@hdld.me>
2019-12-17 15:40:48 +0100

    clean old stuff

--- venv_setup.sh
+++ venv_setup.sh
@@ -8,15 +8,12 @@ rm -rf venv VENV venv.zip   #delete soft links
 mkdir -p $VENV_PATH
 cd $VENV_PATH
 rm -rf venv VENV venv.zip   #delete actual files
-pip3 install --user virtualenv
+python3 -m pip install --user virtualenv
 python3 -m virtualenv venv
 source venv/bin/activate
 pip --version
 pip install -r $PWD_PATH/requirements.txt
 python3 -m spacy download en_core_web_md
-python3 -m nltk.downloader stopwords -d venv/nltk_data
-python3 -m nltk.downloader punkt -d venv/nltk_data
-python3 -m nltk.downloader wordnet -d venv/nltk_data
 virtualenv --relocatable venv
 zip -rq venv.zip venv
 mkdir VENV # Copy for Spark Driver Node:
