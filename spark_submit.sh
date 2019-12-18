3c9063e0ba02a14fdc6405b5cc13553f6427ea36
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-18 12:30:17 +0100

    included 3.5.2 libs

--- spark_submit.sh
+++ spark_submit.sh
@@ -1,16 +1,17 @@
 source venv/bin/activate
 
 # Run Spark Job
-PYSPARK_PYTHON=./VENV/venv/bin/python3 ../spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
+PYSPARK_PYTHON=./VENV/venv/bin/python3 PYTHONPATH=./VENV/venv/build-python ../spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
 --master yarn \
 --conf spark.pyspark.virtualenv.enabled=true \
 --conf spark.pyspark.virtualenv.type=native \
 --conf spark.pyspark.virtualenv.requirements=requirements.txt \
 --conf spark.pyspark.virtualenv.bin.path=./VENV/venv/bin/virtualenv \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
---conf spark.executor.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
---conf spark.driver.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
---conf spark.yarn.am.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64 \
+--conf spark.yarn.appMasterEnv.PYTHONPATH=./VENV/venv/build-python \
+--conf spark.executor.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
+--conf spark.driver.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
+--conf spark.yarn.am.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
 --num-executors 38 --executor-cores 2 --executor-memory 6GB \
 --archives venv.zip#VENV \
 --py-files src/LexVec.py \
