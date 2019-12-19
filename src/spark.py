3c79ac2c5d6584d2dc9366f7d0d4cab11be6b9da
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-20 00:55:09 +0100

    spark file changes and cleanup

--- src/spark.py
+++ src/spark.py
@@ -49,27 +49,23 @@ wsr = WARCSplitReader(sc, input_file.collect())
 wsr.parse_warc_records()
 wsr.process_warc_records()
 warc_stage_rdd = wsr.filter_invalid_records()
-# warc_stage_rdd.cache()
-# print("Processed: {0}".format(warc_stage_rdd.count()))
+warc_stage_rdd.cache()
+print("Processed: {0}".format(warc_stage_rdd.count()))
 
 # Filter to intersting records:
-#if debug:
-recs=[
-    "clueweb12-0000tw-00-00053"
-]
-warc_stage_rdd = warc_stage_rdd.filter(lambda row: row["_id"] in recs)
+if debug:
+    recs=[
+        "clueweb12-0000tw-00-00053"
+    ]
+    warc_stage_rdd = warc_stage_rdd.filter(lambda row: row["_id"] in recs)
 
 print("STAGE 2 - Extracting Text")
 text_prepro = TextExtraction(warc_stage_rdd)
 text_prepro.clean_warc_responses()
 text_prepro.extract_text_from_document()
 txtprepro_stage_rdd = text_prepro.filter_unfit_records()
-# txtprepro_stage_rdd.cache()
-# print("Processed: {0}".format(txtprepro_stage_rdd.count()))
-
-if debug:
-    for row in txtprepro_stage_rdd.collect():
-        print(row["sentences"])
+txtprepro_stage_rdd.cache()
+print("Processed: {0}".format(txtprepro_stage_rdd.count()))
 
 # TODO: Currently Limiting: REMOVE LATER!
 nlp_subset = txtprepro_stage_rdd.take(83)
@@ -80,32 +76,28 @@ ee = EntityRecognition(txtprepro_stage_rdd)
 ee_stage_rdd = ee.extract()
 ee_stage_rdd.cache()
 ee_stage_rdd = ee.join_sentences()
-# ee_stage_rdd.cache()
-# print("Processed: {0}".format(ee_stage_rdd.count()))
-
-if debug:
-    for row in ee_stage_rdd.collect():
-        print(row)
+ee_stage_rdd.cache()
+print("Processed: {0}".format(ee_stage_rdd.count()))
 
 print("STAGE 4 - Entity Linking - Candidate Generation")
 el_cg = ELCandidateGeneration(ee_stage_rdd, es_path, ranking_threshold, model_root_path)
 candidates_rdd = el_cg.get_candidates_from_elasticsearch()
-# candidates_rdd.cache()
-# print("Processed: {0}".format(candidates_rdd.count()))
+candidates_rdd.cache()
+print("Processed: {0}".format(candidates_rdd.count()))
 
 print("STAGE 5 - Entity Linking - Candidate Ranking")
 el_cr = ELCandidateRanking(candidates_rdd, kb_root_path, ranking_threshold, model_root_path)
 el_cr.rank_entity_candidates()
 el_cr.disambiguate_type()
 ranked_candidates_rdd = el_cr.disambiguate_label()
-# el_cr.cache()
-# print("Processed: {0}".format(el_cr.count()))
+ranked_candidates_rdd.cache()
+print("Processed: {0}".format(ranked_candidates_rdd.count()))
 
 print("STAGE 6 - Entity Linking - Mapping Selection")
 el_ms = ELMappingSelection(ranked_candidates_rdd)
 selected_entities_rdd = el_ms.select()
-# selected_entities_rdd.cache()
-# print("Processed: {0}".format(selected_entities_rdd.count()))
+selected_entities_rdd.cache()
+print("Processed: {0}".format(selected_entities_rdd.count()))
 
 print("STAGE 7 - Writing Output")
 ow = OutputWriter(selected_entities_rdd)
