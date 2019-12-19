a6ce6fc665b4e15bcabe7d92d962624bfc1e40f3
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-20 00:16:24 +0100

    setup changes

--- setup.sh
+++ setup.sh
@@ -8,20 +8,34 @@ if [ $? -eq 0 ]; then echo "All done loading hadoop"; else echo "Error in loadin
 module load python/3.6.0    # Load python 3.6.0
 if [ $? -eq 0 ]; then echo "All done loading python3.6"; else echo "Error in loading Python" && exit 1; fi
 
-if [ ! -d "/home/wdps1936/elasticsearch-2.4.1" ]; then
+SCRATCH_PATH='/var/scratch2/wdps1936/'
+mkdir -p $SCRATCH_PATH
+
+if [ ! -d "$SCRATCH_PATH/elasticsearch-2.4.1" ]; then
     echo "Copying Elastic Search folder"
-    cp -r /home/jurbani/wdps/elasticsearch-2.4.1 /home/wdps1936/elasticsearch-2.4.1
+    cp -r /home/jurbani/wdps/elasticsearch-2.4.1 $SCRATCH_PATH/elasticsearch-2.4.1
 fi
 
-if [ ! -d "/home/wdps1936/trident" ]; then
+if [ ! -d "$SCRATCH_PATH/trident" ]; then
     echo "Copying Trident folder"
-    cp -r /home/jurbani/trident /home/wdps1936/trident
+    cp -r /home/jurbani/trident $SCRATCH_PATH/trident
 fi
 
-if [ ! -f "/var/scratch2/wdps1936/lib/model.bin" ]; then
+if [ ! -d "$SCRATCH_PATH/build-python" ]; then
+    echo "Copying Python Bindings for Trident folder"
+    cp -r /home/jurbani/trident/build-python $SCRATCH_PATH/build-python
+fi
+
+if [ ! -d "$SCRATCH_PATH/spark" ]; then
+    echo "Copying Spark folder"
+    cp -r /local/spark $SCRATCH_PATH/spark
+fi
+
+if [ ! -f "$SCRATCH_PATH/lib/model.bin" ]; then
+    mkdir -p $$SCRATCH_PATH/lib
     echo "Downloading lexvec OOV model"
-    wget https://www.dropbox.com/s/buix0deqlks4312/lexvec.commoncrawl.ngramsubwords.300d.W.pos.bin.gz?dl=1 -O /var/scratch2/wdps1936/lib/model.bin.gz
-    gzip -d /var/scratch2/wdps1936/lib/model.bin.gz
+    wget https://www.dropbox.com/s/buix0deqlks4312/lexvec.commoncrawl.ngramsubwords.300d.W.pos.bin.gz?dl=1 -O $SCRATCH_PATH/lib/model.bin.gz
+    gzip -d $SCRATCH_PATH/lib/model.bin.gz
 fi
 
 # Build venv in cluster node
