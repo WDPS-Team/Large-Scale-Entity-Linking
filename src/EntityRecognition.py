e7889cc48653351ddc1587b472e93cb11ae0d0c9
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-20 03:25:18 +0100

    ELCG clean

--- src/EntityRecognition.py
+++ src/EntityRecognition.py
@@ -6,6 +6,11 @@ class EntityRecognition:
         self.warc_docs = warc_docs
 
     def extract(self):
+        """
+            Named Entity Recognition using 'en_core_web_md' model of spaCy.
+            - Data is provided to spaCy in paragraphs for better entity recognition
+            - Numerical values are ignored for improving the quality of the entities recognised
+        """
         def process(row):
             spacy_nlp = spacy.load("en_core_web_md")
             
@@ -15,7 +20,7 @@ class EntityRecognition:
                 for element in document.ents:
                     text = element.text.strip("\n").replace("\n", "").replace("\r", "")
                     if element.label_ not in ["CARDINAL", "DATE", "QUANTITY", "TIME", "ORDINAL", "MONEY", "PERCENT", "QUANTITY"]:
-                        entity = dict(type=element.label_, text=text)
+                        entity = { "type" : element.label_, "text":text }
                         entity_list.append(entity)
                 return entity_list
             entities = [spacy_extract(paragraph) for paragraph in row["paragraphs"]]
