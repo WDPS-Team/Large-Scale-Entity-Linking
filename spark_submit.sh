1c809ff62a51090b037e6619137f043aa9c79975
WDPS course <abijithanikkuruthi@gmail.com>
2019-12-20 08:28:13 +0100

    memory requirements

--- spark_submit.sh
+++ spark_submit.sh
@@ -12,7 +12,7 @@ PYSPARK_PYTHON=./VENV/venv/bin/python3 PYTHONPATH=./VENV/venv/build-python /var/
 --conf spark.executor.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
 --conf spark.driver.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
 --conf spark.yarn.am.extraLibraryPath=/cm/shared/package/gcc/6.4.0/lib64:/cm/shared/package/python/3.5.2/lib \
---num-executors 14 --executor-cores 6 --executor-memory 20GB \
+--num-executors 12 --executor-cores 5 --executor-memory 24GB \
 --archives venv.zip#VENV \
 --py-files src/LexVec.py,src/ELCandidateRanking.py \
 src/spark.py --es "$1" --f "$2" --hdfsout "$3"
